c69805d2338a907bbfe4782109034a52e0f9b5dc
Isma <ismaharo>
2015-08-22 13:59:35 -0500

    Primer commit

---
+++ js/API/archivos.js
@@ -0,0 +1,59 @@
+var f = {
+	txt: null,
+
+	action: null,
+
+	createFile: function(){
+		f.txt = $('#aSend').val();
+		f.action = 0;
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, f.gotFS, f.fail);
+
+	},
+
+	readFile: function(){
+		f.action = 1;
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, f.gotFS, f.fail);
+	},
+
+	//-------- FUNCIONES PARA ESCRIBIR Y LEER ----------
+
+	gotFS: function(fileSystem) {
+		if(f.action == 0 ){
+			// Vamos a escribir
+			fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, f.gotFileEntry, f.fail);
+		
+		}else{
+			// Vamos a leer
+			 fileSystem.root.getFile("readme.txt", null, f.gotFileEntry, f.fail);
+		}
+    },
+    
+    gotFileEntry: function(fileEntry) {
+    	if(f.action == 0){
+    		// Vamos a escribir
+        	fileEntry.createWriter(f.gotFileWriter, f.fail);
+    	}else{
+    		// Vamos a leer
+    		fileEntry.file(f.gotFileReader,f.fail);
+    	}
+    },
+
+    gotFileWriter: function(writer) {
+        writer.onwriteend = function(evt) {
+        	alert("Archivo escrito");
+        };
+        writer.write(f.txt);
+    },
+
+	gotFilteReader: function(file){
+		var reader = new FileReader();
+        reader.onloadend = function(evt) {
+            $('#aGet').txt(evt.target.result);
+        };
+        reader.readAsText(file);
+	},
+
+    fail: function(error) {
+        alert(error.code);
+    }
+};
---
+++ js/actions.js
@@ -0,0 +1,27 @@
+var jQT = new $.jQTouch({
+	themeSelectionSelector: '#jqt'
+});
+
+var fn = {
+	ready: function(){
+		document.addEventListener("deviceready", fn.init, false);
+	},
+	init: function(){
+		$('#ncSend').tap(fn.crearContacto);
+		$('#cListar').tap(cn.findContacts);
+		$('#aEscribir').tap(f.createFile);
+		$('#aLeer').tap(f.readFile);
+	},
+	crearContacto: function(){
+		var n = $('#ncNom').val();
+		var t = $('#ncTel').val();
+		var m = $('#ncMail').val();
+		
+		if(n != '' && t != '' && m != '')
+			cn.createContact(n,t,m);
+		else
+			alert("Todos los campos son requerido");
+	}
+};
+
+$(fn.ready);
